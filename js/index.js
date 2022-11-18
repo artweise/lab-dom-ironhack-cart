4623f74086da2561eb3feff76fed21903caa6d92
artweise <art2wsxweise7ujm@gmail.com>
2022-11-18 19:24:11 +0100

    done iterations 1-3

--- js/index.js
+++ js/index.js
@@ -1,23 +1,58 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  // console.log(product);
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  const subTotal = product.querySelector('.subtotal span');
 
-  //... your code goes here
+  const subTotalByItem = Number(price * quantity);
+  // const subTotalByItem = Number(price * quantity).toFixed(2); //--> works, makes the subtotal with .00
+  //BUT the Total is very strange $00.000.000.00 or with numbers when I give the quantity, Why???
+
+  subTotal.innerHTML = subTotalByItem;
+
+  // console.log(price);
+  // console.log(quantity);
+  // console.log(subTotal);
+
+  // console.log(typeof price);
+  // console.log(typeof quantity);
+  // console.log(typeof subTotal);
+
+  return subTotalByItem;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+  // console.log(allProducts);
+  const myProductsArray = [...allProducts];
+
+  let sumForEachItem = 0;
+  myProductsArray.forEach((element) => {
+    sumForEachItem = updateSubtotal(element);
+  });
+  //another approach with for of loop
+  // for (let element of allProducts) {
+  //   totalSum += updateSubtotal(element);
+  // }
 
   // ITERATION 3
-  //... your code goes here
+  let totalSum = 0;
+  myProductsArray.forEach((element) => {
+    totalSum += updateSubtotal(element);
+  });
+  const total = document.querySelector('#total-value span');
+
+  total.innerHTML = totalSum;
 }
 
 // ITERATION 4
